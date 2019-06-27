e1b8b3371e06894f4f4a3c1a200d4172fe356315
QueraltSosaMompel <queraltsosa@hotmail.com>
2019-06-27 19:11:39 +0100

    Stategy 1 was updated

--- Bag.java
+++ Bag.java
@@ -11,20 +11,12 @@ public class Bag {
     public int getStrategy1(List<Item> items) {
         int finalWeight = 0;
         int finalPrice = 0;
-        for (int i = 0; i<items.size(); i++) {
-            int actualWeight = (items.get(i)).getWeight();
-            Item res = items.get(i);
-            for (int j = i+1; j<items.size(); j++) {
-                int nextWeight = (items.get(j)).getWeight();
-                if (actualWeight < nextWeight) {
-                    res.setWeight(nextWeight);
-                    res.setPrice((items.get(j)).getPrice());
-                }
-            }
-            if (finalWeight + res.getWeight() <= capacity) {
-                finalWeight += res.getWeight();
-                finalPrice += res.getPrice();
-                System.out.println(res.getWeight());
+        Collections.sort(items, Collections.reverseOrder());
+        for (Item item : items) {
+            if (item.getWeight() + finalWeight <= capacity) {
+                finalWeight += item.getWeight();
+                finalPrice += item.getPrice();
+                System.out.println(item.getWeight());
             }
         }
         return finalPrice;
